64fa1c2439387b57962f2091a305311c0beedccc
Abdur Rahman <rahman.primez@live.com>
2019-08-19 23:33:08 +0600

    added a basic hash table from https://codepen.io/beaucarnes/pen/VbYGMb?editors=0012

---
+++ Hash Table/index.js
@@ -0,0 +1,73 @@
+var hash = (string, max) => {
+    var hash = 0;
+    for (var i = 0; i < string.length; i++) {
+        hash += string.charCodeAt(i);
+    }
+    return hash % max;
+}
+
+let HashTable = function() {
+
+    let storage = [];
+    const storageLimit = 14;
+
+    this.print = function() {
+        console.log(storage)
+    }
+
+    this.add = function(key, value) {
+        var index = hash(key, storageLimit);
+        if (storage[index] === undefined) {
+            storage[index] = [
+                [key, value]
+            ];
+        } else {
+            var inserted = false;
+            for (var i = 0; i < storage[index].length; i++) {
+                if (storage[index][i][0] === key) {
+                    storage[index][i][1] = value;
+                    inserted = true;
+                }
+            }
+            if (inserted === false) {
+                storage[index].push([key, value]);
+            }
+        }
+    };
+
+    this.remove = function(key) {
+        var index = hash(key, storageLimit);
+        if (storage[index].length === 1 && storage[index][0][0] === key) {
+            delete storage[index];
+        } else {
+            for (var i = 0; i < storage[index].length; i++) {
+                if (storage[index][i][0] === key) {
+                    delete storage[index][i];
+                }
+            }
+        }
+    };
+
+    this.lookup = function(key) {
+        var index = hash(key, storageLimit);
+        if (storage[index] === undefined) {
+            return undefined;
+        } else {
+            for (var i = 0; i < storage[index].length; i++) {
+                if (storage[index][i][0] === key) {
+                    return storage[index][i][1];
+                }
+            }
+        }
+    };
+
+};
+
+
+let ht = new HashTable();
+ht.add('name', 'person');
+ht.add('anme', 'riyad');
+
+console.log(ht.lookup('name'));
+
+// ht.print();
